4c9a9a5a68abae8e351e1c2910b40fd269a2fc84
Roody <rjointer2@gmail.com>
2021-05-06 20:22:53 -0700

    pushing

--- backend/Controllers/API/users.js
+++ backend/Controllers/API/users.js
@@ -5,15 +5,48 @@ const route = require('express').Router();
 // To get all users
 
 route.get('/', (req, res) => {
-    User
+    User.findAll().then((users) => {
+        res.status(201).send(users)
+    })
 })
 
 // To get user by id
 
+route.get('/id', (req, res) => {
+    User.findByPk(req.params.id).then((user) => {
+        res.status(201).send(user)
+    }).catch((err) => {
+        res.status(500).send({
+            error: err
+        })
+    })
+});
+
 // To create new user
+
+route.post('/', (req, res) => {
+    User.create({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        password: req.body.password,
+    }).then((user) => {
+        res.status(201).send(user);
+        console.log(user)
+    })
+});
 
 // To delete user
 
+route.delete('/:id', (req, res) => {
+    User.destory({
+        where: { id: req.params.id }
+    })
+    res.status(201).send('user delete?')
+})
+
 // to update user
+
+route.put('/:id')
 
 // error handles
