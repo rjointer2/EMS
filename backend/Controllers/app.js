ab23e16256e777a7709a34798ae916b664a2eec9
Roody <rjointer2@gmail.com>
2021-05-12 15:35:11 -0700

    staging

--- backend/Controllers/app.js
+++ backend/Controllers/app.js
@@ -7,6 +7,8 @@ const route = require('express').Router();
 
 // THIS WILL SEND THE APPLICATION WHEN THE BUILD IS RAN
 
+ 
+
 route.get('/', (req, res) => {
     // THE TOKE HAS TO SENT THROUGH HERE WHEN THE LOGI AUTH SUCCESS 
     req.session.viewCount += 1
