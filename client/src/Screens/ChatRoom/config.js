750212216c640d5ef80761cffa2b8064327ca862
Muh <mmtariqk@gmail.com>
2021-05-15 12:15:19 -0700

    ChatRoom

---
+++ client/src/Screens/ChatRoom/config.js
@@ -0,0 +1,6 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/YOUR_TOKEN/token";
+const instanceLocator = "YOUR_KEY";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
