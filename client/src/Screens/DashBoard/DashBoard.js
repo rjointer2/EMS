63511c2231fd7e6c49471772ec0f08eb20764420
Roody <rjointer2@gmail.com>
2021-05-17 00:50:32 -0700

    time for a break

--- client/src/Screens/DashBoard/DashBoard.js
+++ client/src/Screens/DashBoard/DashBoard.js
@@ -1,33 +1,25 @@
-/* 
-
-    GOALS
-
-    Admin can can only see this
-    ->  Function that if the user.property is admin or admin set to true then the sidebar
-        will display the dashboard
 
 
-        TABLE IS CREATED WHEN A USER IS FETCH FOR EACH USER
-    ->  Add, Remove and Update tasks for employees
-
-    Example Table
-
-    USER 
-    |____SHOW TASK____EDIT TASK____REMOVE TASK____TASK__STATUS
-
-    For right now, we don't need to worry about redux/react-redux
-    It doesn't matter exactly what the user's data is... what matters is how the user's data is handled
-
-
-
-*/
-
-import { Container, Grid, Paper, Typography } from '@material-ui/core';
+import { Container, Grid, Paper, TextField, Typography } from '@material-ui/core';
+import { useState } from 'react';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './DashBoardStyle.js';
 import { DashBoardChild, FlexUserTable } from './DashBoardStyle.js';
 
 const DashBoard = () => {
+
+    // blank form object to be out base state
+
+    const initialState = {
+        firstName: '',
+        lastName: '',
+        department: '',
+        picture: '',
+        recovery: ''
+    }
+
+    // use to update the state 
+    const [value, setValue] = useState('')
 
 
     const users = ['bob', 'bill']
@@ -55,6 +47,22 @@ const DashBoard = () => {
                     <br/><br/>
                     Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Vestibulum lectus mauris ultrices eros in cursus turpis. Non arcu risus quis varius quam quisque id diam vel. Massa placerat duis ultricies lacus sed turpis tincidunt id. 
                 </Typography>
+
+
+                <form>
+
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <TextField variant="outlined" label="First Name" value={value.firstName}/>
+                            <TextField variant="outlined" label="Last Name" value={value.lastname}/>
+                            <TextField variant="outlined" label="Picture" value={value.picture}/>
+                            <TextField variant="outlined" label="Department" value={value.department}/>
+                            <TextField variant="outlined" label="Recovery" value={value.recovery}/>
+                        </Grid>
+                        <Grid item xs={6}></Grid>
+                    </Grid>
+
+                </form>
 
 
                 {/* Just use flex box row */}
